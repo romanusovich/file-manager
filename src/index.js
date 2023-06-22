3d3b69d92fd28718f4e700498520ef53be7835b9
romanusovich1 <romanusovich1@gmail.com>
2023-06-22 13:50:44 +0300

    feat: add up, cd, ls commands

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import process from 'process';
 import { homedir } from 'os';
+import fs from 'fs';
+import path from 'path';
 
 function start() {
     const USER_NAME = process.argv.slice(2)[0].split('=').reverse()[0];
-    const CUURENT_DIR = homedir();
+    let currentDir = homedir();
     process.stdout.write(`\nWelcome to the File Manager, ${USER_NAME}!\n`);
+    process.stdout.write(`You are currently in ${currentDir}\n`);
 
     process.stdin.on('data', (data) => {
-        if (data.toString().trim() === '.exit') process.exit();
+        const COMMAND = data.toString().trim().split(' ')[0];
+        const ARGUMENTS = data.toString().trim().split(' ').slice(1);
+        if (COMMAND === '.exit') process.exit();
         else {
-            
+            switch (COMMAND) {
+                case 'up':
+                    if (currentDir.split('\\').length > 1)
+                        currentDir = currentDir.split('\\').slice(0, -1).join('\\');
+                    break;
+                case 'cd':
+                    currentDir = path.join(currentDir, ARGUMENTS[0]);
+                    break;
+                case 'ls':
+                    process.stdout.write('(index)\t\tName\t\tType\n');
+                    fs.readdir(currentDir, { withFileTypes: true }, (err, files) => {
+                        files.forEach((file, i) => {
+                            process.stdout.write(`${i}\t\t${file.name}\t\t${file.isFile() ? 'file' : 'directory'}\n`);
+                        });
+                    });
+                    break;
+                default:
+                    process.stdout.write('Invalid input\n');
+                    break;
+            }
+
+            process.stdout.write(`You are currently in ${currentDir}\n`);
         }
     });
 
